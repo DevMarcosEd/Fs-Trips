7ee8065b5fd332cbe786c1b72e7dd018566be5e1
devmarcos <devmarcoseduardo@gmail.com>
2023-07-31 21:12:08 -0300

    chore: add next auth prisma config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+// Importação dos módulos e dependências necessárias
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
+
+// Importação da instância do Prisma
+import { prisma } from "@/lib/Prisma";
+
+// Configuração das opções de autenticação para o NextAuth.js
+export const authOptions: AuthOptions = {
+    // Definição do adaptador Prisma para armazenar as sessões de autenticação
+    adapter: PrismaAdapter(prisma) as Adapter,
+    // Configuração dos provedores de autenticação, neste caso, somente o Google
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!, // ID do cliente Google obtido do ambiente
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!, // Segredo do cliente Google obtido do ambiente
+        }),
+    ]
+};
+
+// Criação do gerenciador de autenticação usando as opções configuradas
+const handler = NextAuth(authOptions);
+
+// Exportação do gerenciador de autenticação para o Next.js tratar as requisições GET e POST
+export { handler as GET, handler as POST };
