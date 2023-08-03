620e9db5c34c4925ae4988dcd1693cad1db63603
devmarcos <devmarcoseduardo@gmail.com>
2023-08-03 11:58:26 -0300

    feat: add initial search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 
+import TripSearch from "@/components/TripSearch/TripSearch"
+
 export default function Home() {
   return (
     <>
-      Hello Wolrd
+      <TripSearch />
     </>
   )
 }
