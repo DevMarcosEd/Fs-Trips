b1d5c3d112a2a208a0f5ef7652a9f8ad00b57584
devmarcos <devmarcoseduardo@gmail.com>
2023-08-05 20:42:08 -0300

    feat: add quick search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import QuickSearch from "@/components/QuickSearch/QuickSearch"
 import TripSearch from "@/components/Search/TripSearch"
 
 export default function Home() {
   return (
     <>
       <TripSearch />
+      <QuickSearch />
     </>
   )
 }
