b26cd82426272f3c33ed7aec42e2c86131b21e67
devmarcos <devmarcoseduardo@gmail.com>
2023-08-28 20:28:23 -0300

    feat: add recommended trips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import QuickSearch from "@/components/QuickSearch/QuickSearch"
-import TripSearch from "@/components/Search/TripSearch"
+import QuickSearch from "@/components/QuickSearch/QuickSearch";
+import RecommendedTrips from "@/components/RecommendedTrips/RecommendedTrips";
+import TripSearch from "@/components/Search/TripSearch";
 
 export default function Home() {
   return (
     <>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </>
-  )
+  );
 }
