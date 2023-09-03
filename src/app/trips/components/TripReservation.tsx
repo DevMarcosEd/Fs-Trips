e20d0bfd45a32c74391a072f506b44343dc3be98
devmarcos <devmarcoseduardo@gmail.com>
2023-09-03 18:57:27 -0300

    feat: add required validation to trip reservation

--- src/app/trips/components/TripReservation.tsx
+++ src/app/trips/components/TripReservation.tsx
@@ -6,19 +6,86 @@ import Input from "@/components/Inputs/Input";
 import { Trip } from "@prisma/client";
 import React from "react";
 
+import { useForm, Controller } from "react-hook-form";
+
 interface TripReservationProps {
     trip: Trip;
 }
 
+interface TripReservationForm {
+    guests: number;
+    startDate: Date | null;
+    endDate: Date | null;
+}
+
 const TripReservation = ({ trip }: TripReservationProps) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        control
+    } = useForm<TripReservationForm>();
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
     return (
         <div className="flex flex-col px-5">
             <div className="flex gap-4">
-                <DataPicker placeholderText="Data de Início" onChange={() => { }} className="w-full" />
-                <DataPicker placeholderText="Data Final" onChange={() => { }} className="w-full" />
+                <Controller
+                    name="startDate"
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "Data inical é obrigatória"
+                        },
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DataPicker
+                            error={!!errors?.startDate}
+                            errorMessage={errors?.guests?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data de Início"
+                            className="w-full"
+                        />
+                    )}
+                />
+
+                <Controller
+                    name="endDate"
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "Data final é obrigatória"
+                        }
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DataPicker
+                            error={!!errors?.endDate?.message}
+                            errorMessage={errors?.guests?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data Final"
+                            className="w-full"
+                        />
+                    )}
+                />
             </div>
 
-            <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests} )`} className="mt-4" />
+            <Input {...register("guests", {
+                required: {
+                    value: true,
+                    message: "Número de hóspedes é obrigatório"
+                }
+            })} placeholder={`Número de hóspedes (max: ${trip.maxGuests} )`}
+                className="mt-4"
+                error={!!errors.guests?.message}
+                errorMessage={errors?.guests?.message}
+            />
 
             <div className="flex justify-between mt-2 ">
                 <p className="font-medium text-sm text-primaryDarker">Total: </p>
@@ -26,7 +93,7 @@ const TripReservation = ({ trip }: TripReservationProps) => {
             </div>
 
             <div className="pb-10 border-b border-grayLighter w-full">
-                <Button className="mt-3 w-full">Reservar agora</Button>
+                <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">Reservar agora</Button>
             </div>
         </div>
     );
