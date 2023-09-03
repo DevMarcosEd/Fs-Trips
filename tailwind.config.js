4af4879f3c0a1dff792907fb0590a3320c1eba3d
devmarcos <devmarcoseduardo@gmail.com>
2023-09-03 11:45:26 -0300

    feat: add footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'search-background': 'url(/world-map.png)'
+        "search-background": "url(/world-map.png)"
       },
       colors: {
-        primary: '#590BD8',
-        primaryLighter: 'DDD5EA',
-        primaryDarker: '#312A4F',
-        grayPrimary: '#717171',
-        grayLighter: '#BBBFBF'
+        primary: "#590BD8",
+        primaryLighter: "#717171", //DDD5E9
+        primaryDarker: "#312A4F",
+        grayPrimary: "#717171",
+        grayLighter: "#BBBFBF",
+        walterWhite: "#f5f5f5"
       },
       textColor: {
-        dark: '#717171',
+        dark: "#717171",
       }
     },
   },
   plugins: [],
-}
+};
